688f09dabc5b2fc178f6ef4189994307028e8838
nouremak <nour@emak.tech>
2025-08-25 23:48:18 +0300

    add the optio to have a match count

--- search-text/src/components/Search.tsx
+++ search-text/src/components/Search.tsx
@@ -3,7 +3,7 @@ import React, { useState, ChangeEvent } from "react";
 function Search(){
     const [ searchText, setSearchText ] = useState<string>("");
 
-    const text: string = "My Name is Nour";
+    const text: string = "My Name is Nour Nour Nour";
 
     const getSearchTextHilights = (text: string, highlight: string) => {
         if (!highlight) return text;
@@ -22,14 +22,26 @@ function Search(){
         return parts
     }
 
+    const getCountText = (text: string, highlight: string): any => {
+        if (!highlight) return 0;
+        const regex = new RegExp(highlight, "gi");
+        return (text.match(regex) || []).length;
+    }
+
     const handleChangeText = (e : ChangeEvent<HTMLInputElement>) => {
         setSearchText(e.target.value);
     }
     
+    const matchCountText = getCountText(text, searchText)
     return (
-        <div>
-            <h1>Search example</h1>
-            <input type="text" placeholder="Search here ..." value={searchText} onChange={handleChangeText}/>
+        <div className="container">
+            <h1 className="heading-one">Search example</h1>
+            <input className="input-text" type="text" placeholder="Search here ..." value={searchText} onChange={handleChangeText}/>
+            {searchText &&(
+                <div>
+                    {matchCountText} were found.{matchCountText !== 1 ? "es" : ""}
+                </div>
+            )}
             <p>{getSearchTextHilights(text, searchText)}</p>
         </div>
     )
